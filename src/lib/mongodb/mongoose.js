07e0c8502968aaa9b7242fa11229942c8d4c6f61
Adnan Ghani <agadnanrocks07@gmail.com>
2025-06-22 20:11:06 +0530

    Connection to mongoDB and sending clerk id as well as creating database done

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -7,7 +7,7 @@ export const connect = async () => {
     return;
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       dbName: 'cinepulse',
       useNewUrlParser: true,
       useUnifiedTopology: true,
