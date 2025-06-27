98a35678d02db3a0a3ce91a6bf7a34e3066d1250
Adnan Ghani <agadnanrocks07@gmail.com>
2025-06-27 21:17:04 +0530

    Updated Movie Card for TV Show Routing

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -5,7 +5,8 @@ import MovieModal from "@/components/MovieModal"; // import the modal
 const API_KEY = process.env.API_KEY;
 
 export default async function MovieContentPage({ params }) {
-  const { id } = params;
+  const { id } = await params;
+  console.log(params);
 
   const movieRes = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
